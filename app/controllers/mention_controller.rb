12ae2bc5a3d7ba60ed4c5608992902a5b360ff3f
Codrut Constantin Gusoi <codrut.gusoi@zitec.ro>
2015-03-20 13:23:57 +0200

    Bump version, compatible with redmine 3

--- app/controllers/mention_controller.rb
+++ app/controllers/mention_controller.rb
@@ -4,9 +4,9 @@ class MentionController < ApplicationController
     project_identifier = params[:project_identifier]
     users = []
     if project_identifier
-      users = User.with_name(params[:search_tag]).includes(members: :project).where('projects.identifier = ?', project_identifier)
+      users = User.with_name(params[:search_tag]).joins(members: :project).where('projects.identifier = ?', project_identifier)
     elsif issue_id
-      users = User.with_name(params[:search_tag]).includes(members: {project: :issues}).where('issues.id = ?', issue_id)
+      users = User.with_name(params[:search_tag]).joins(members: {project: :issues}).where('issues.id = ?', issue_id)
     end
     result = users.each.inject([]) do |data, user|
       data << {id: user.id, username: user.login, full_name: user.name, email: user.mail} unless user.locked?
--- config/routes.rb
+++ config/routes.rb
@@ -1,1 +1,1 @@
-match 'mention/search', :to => 'mention#search'
+get 'mention/search', to: 'mention#search'
